a275803e1cfe2305a590d8ec244e4e6b2d65b765
2DAW10 <2DAW10@DOMDPTOINFJMPP.INF>
2025-01-27 18:08:25 +0000

    cambioslunes27

--- script.js
+++ script.js
@@ -3,11 +3,13 @@
 // Problema: Falta de manejo de eventos básicos y retroalimentación visual
 
 document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Problema: No se avisa al usuario que el formulario no se envió
+   // event.preventDefault(); // Problema: No se avisa al usuario que el formulario no se envió
 let form=document.querySelector("form");
+
 for (const element of form) {
     if(element.value==""){
-        element.value="falta campo"
+      let aviso=document.createElement("p");
+      aviso.innerText="campo obligatorio";
     }
 }
     const name = document.getElementById('name').value;
@@ -18,8 +20,13 @@ for (const element of form) {
 
     if (!name || !email || !message) {
         alert('Por favor, complete todos los campos.'); // Problema: Mensaje genérico y poco informativo
+     
+       
+        console.log(doc)
+    
     } else {
         console.log('Formulario enviado:', { name, email, message });
         alert('Gracias por contactarnos.'); // Problema: No redirige ni proporciona confirmación visual en la página
+    
     }
 });
